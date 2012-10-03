5977e250cc805825a77829dbe5ef4f398abdd34e
Shane <sir.reiko@gmail.com>
2012-10-04 01:47:35 +1000

    Set up precompiled headers
    Set up precompiled headers and an entry point for the program.

--- src/Paddocks/StdAfx.h
+++ src/Paddocks/StdAfx.h
@@ -19,3 +19,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include "windows.h"
 #endif
+
+#include <cstdlib>
+#include <vld.h>
--- src/Paddocks/main.cpp
+++ src/Paddocks/main.cpp
@@ -1,4 +1,18 @@
-int main ()
+/*************************************************************************
+ Paddocks
+ by Shane "Reiko" Swenson - October 2012.
+
+ Computer version of the pen and paper game, 'Paddocks' also known as
+ 'Dots and Boxes'.
+	
+ https://github.com/Reikooters/Paddocks
+**************************************************************************
+ main.cpp
+**************************************************************************
+ Contains the entry point of the program.
+**************************************************************************/
+
+int main (int argc, char **argv)
 {
-	return 0;
+	return EXIT_SUCCESS;
 }
