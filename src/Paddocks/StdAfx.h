4474ab0ec623700f03f3627cbdd7181bda23c111
Shane <sir.reiko@gmail.com>
2012-10-07 05:42:40 +1100

    VLD now only included in Windows builds
    Visual Leak Detector now only included in Windows builds.

--- src/Paddocks/StdAfx.h
+++ src/Paddocks/StdAfx.h
@@ -19,9 +19,9 @@
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
 #include "windows.h"
+#include <vld.h>
 #endif
 
 #include <cstdlib>
 #include <memory>
 #include <exception>
-#include <vld.h>
