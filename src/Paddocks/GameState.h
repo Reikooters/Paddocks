ed5663078c26fd46ebe4442ab882263ead70bf3c
Shane <sir.reiko@gmail.com>
2012-10-14 08:12:39 +1100

    Added GameState abstract class.

---
+++ src/Paddocks/GameState.h
@@ -0,0 +1,27 @@
+#pragma once
+/*************************************************************************
+ * Paddocks
+ * by Shane "Reiko" Swenson - October 2012.
+ * 
+ * Computer version of the pen and paper game, 'Paddocks' also known as
+ * 'Dots and Boxes'.
+ * 	
+ * https://github.com/Reikooters/Paddocks
+ *************************************************************************
+ * GameState.h
+ *************************************************************************/
+
+
+/*************************************************************************
+ * class GameState
+ *************************************************************************
+ * Abstract class which game states will derive from.
+ *************************************************************************/
+class GameState
+{
+public:
+	virtual ~GameState() {}
+	virtual void enter() = 0;
+	virtual void update(Ogre::Real deltaTimeSecs) = 0;
+	virtual void exit() = 0;
+};
